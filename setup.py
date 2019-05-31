4ad6b87909bfb6ea387a5ffabf7ed25a461b7daa
Marc Wolff <mwolff.mw@gmail.com>
2019-05-31 14:53:33 -0400

    Consolidated all files in a single one Created the package

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+	name='consoleprint',
+	version='0.1',
+	author='Marc Wolff',
+	author_email='mwolff.mw@gmail.com',
+	description='A library for convenient handling of colored console prints',
+	url='https://github.com/wolffmarc/consoleprint',
+	packages=setuptools.find_packages()
+)
